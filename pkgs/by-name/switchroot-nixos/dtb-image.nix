fad83c95e12c8aaf4a470a05ab07bba92e0e7772
Waldemar Tomme <waldetomme@gmail.com>
2025-04-16 21:50:17 +0200

    Build switchroot package

---
+++ pkgs/by-name/switchroot-nixos/dtb-image.nix
@@ -0,0 +1,11 @@
+{ kernel
+, runCommand
+, android-tools
+}:
+runCommand "nx-plat.dtimg" {} ''
+  ${android-tools}/bin/mkdtboimg create $out --page_size=1000 \
+    ${kernel}/dtbs/tegra210-odin.dtb --id=0x4F44494E \
+    ${kernel}/dtbs/tegra210b01-odin.dtb --id=0x4F44494E --rev=0xb01 \
+    ${kernel}/dtbs/tegra210b01-vali.dtb --id=0x56414C49 \
+    ${kernel}/dtbs/tegra210b01-fric.dtb --id=0x46524947
+''
--- pkgs/by-name/switchroot-nixos/package.nix
+++ pkgs/by-name/switchroot-nixos/package.nix
@@ -1,4 +1,6 @@
-{ callPackage }:
+{ callPackage
+, runCommand
+}:
 {
   kernel,
   initialRamdisk,
@@ -8,5 +10,15 @@ let
   uImage = callPackage ./uImage.nix { inherit kernel; };
   uInitrd = callPackage ./uInitrd.nix { inherit initialRamdisk; };
   boot-scr = callPackage ./boot-scr.nix { inherit toplevel; };
+  dtb-image = callPackage ./dtb-image.nix { inherit kernel; };
+
+  package = runCommand "install-package" {} ''
+    mkdir -p $out/switchroot/nixos
+
+    cp ${uInitrd} $out/switchroot/nixos/initramfs
+    cp ${uImage} $out/switchroot/nixos/uImage
+    cp ${boot-scr} $out/switchroot/nixos/boot.scr
+    cp ${dtb-image} $out/switchroot/nixos/nx-plat.dtimg
+  '';
 in
-  boot-scr
+  package
