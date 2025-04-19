2e8685ec81d9ae797239b906d6cae10759fd9e9b
Waldemar Tomme <waldetomme@gmail.com>
2025-04-19 13:14:19 +0200

    Add script to copy closure to SD card

--- pkgs/by-name/switchroot-nixos/package.nix
+++ pkgs/by-name/switchroot-nixos/package.nix
@@ -1,5 +1,9 @@
 { callPackage
 , runCommand
+, writeShellApplication
+, closureInfo
+
+, rsync
 }:
 {
   kernel,
@@ -12,7 +16,30 @@ let
   boot-scr = callPackage ./boot-scr.nix { inherit toplevel; };
   dtb-image = callPackage ./dtb-image.nix { inherit kernel; };
 
-  package = runCommand "install-package" {} ''
+  closure-info = closureInfo {
+    rootPaths = [ toplevel ];
+  };
+
+  copy-closure = writeShellApplication {
+    name = "copy-closure-to";
+    runtimeInputs = [
+      rsync
+    ];
+    text = ''
+      TARGET="$1"
+      # shellcheck disable=SC2046
+      sudo rsync -auh --info=progress2 $(cat ${closure-info}/store-paths) "$TARGET/nix/store"
+      sudo cp ${closure-info}/registration "$TARGET/copy-closure-registration"
+      sudo nixos-enter --root "$TARGET" --command "nix-store --load-db /copy-closure-registration"
+      sudo rm "$TARGET/copy-closure-registration"
+    '';
+  };
+
+  package = runCommand "switchroot-boot" {} ''
+    mkdir -p $out/misc
+    ln -s ${closure-info} $out/misc/closure-info
+    ln -s ${copy-closure}/bin/copy-closure-to $out/misc/
+
     mkdir -p $out/switchroot/nixos
 
     cp ${uInitrd} $out/switchroot/nixos/initramfs
