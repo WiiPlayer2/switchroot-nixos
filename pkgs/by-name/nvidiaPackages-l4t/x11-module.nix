6c22960b048b02a8a57bdfb6e69e8b18f0605375
Waldemar Tomme <waldetomme@gmail.com>
2025-04-21 18:58:45 +0200

    Patch extension library as well

--- pkgs/by-name/nvidiaPackages-l4t/x11-module.nix
+++ pkgs/by-name/nvidiaPackages-l4t/x11-module.nix
@@ -11,8 +11,8 @@ runCommand "nvidia-l4t-x11-module" {} ''
     --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
     --set-rpath ${iconv}/lib:${tegra-lib}/lib \
     $out/lib/xorg/modules/drivers/nvidia_drv.so
+  patchelf \
+    --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+    --set-rpath ${iconv}/lib:${tegra-lib}/lib \
+    $out/lib/xorg/modules/extensions/libglxserver_nvidia.so
 ''
-# libnvidia-rmapi-tegra.so.32.6.1
-# libm.so.6
-# libc.so.6
-# libdl.so.2
