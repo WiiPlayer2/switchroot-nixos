46bca2eb1289513e693865ec009750420335dd69
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-04-23 14:05:11 +0200

    Add icd files

--- pkgs/by-name/nvidiaPackages-l4t/tegra-lib.nix
+++ pkgs/by-name/nvidiaPackages-l4t/tegra-lib.nix
@@ -1,5 +1,6 @@
 { nvidia-drivers
 , runCommand
+, writers
 , stdenv
 , autoPatchelfHook
 , libgcc
@@ -14,41 +15,67 @@
 , wayland-scanner
 , egl-wayland
 }:
-# runCommand "nvidia-l4t-tegra-lib" {} ''
-#   mkdir -p $out/lib
-#   cp --no-preserve=mode -r ${nvidia-drivers}/usr/lib/aarch64-linux-gnu/tegra/* $out/lib/
-# ''
-stdenv.mkDerivation {
-  name = "nvidia-l4t-tegra-lib";
-  version = "32.6.1";
+let
+  library-files = stdenv.mkDerivation {
+    name = "nvidia-l4t-tegra-lib-files";
+    version = nvidia-drivers.version;
 
-  src = nvidia-drivers;
+    src = nvidia-drivers;
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
 
-  buildInputs = [
-    libgcc.lib
-    libglvnd
-    glib
-    alsa-lib
-    cairo
-    pango
-    gtk3
-    libv4l
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    wayland-scanner
-    egl-wayland
-  ];
+    buildInputs = [
+      libgcc.lib
+      libglvnd
+      glib
+      alsa-lib
+      cairo
+      pango
+      gtk3
+      libv4l
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+      wayland-scanner
+      egl-wayland
+    ];
 
-  installPhase = ''
-    mkdir -p $out/lib
-    runHook preInstall
-    cp --no-preserve=mode -r usr/lib/aarch64-linux-gnu/tegra/* $out/lib/
-    cp --no-preserve=mode -r usr/lib/aarch64-linux-gnu/tegra-egl/* $out/lib/
-    ln -s $out/lib/libcuda.so.1.1 $out/lib/libcuda.so.1
-    runHook postInstall
+    installPhase = ''
+      mkdir -p $out/lib
+      runHook preInstall
+      cp --no-preserve=mode -r usr/lib/aarch64-linux-gnu/tegra/* $out/lib/
+      cp --no-preserve=mode -r usr/lib/aarch64-linux-gnu/tegra-egl/* $out/lib/
+      ln -sf $out/lib/libcuda.so.1 $out/lib/libcuda.so
+      ln -sf $out/lib/libcuda.so.1.1 $out/lib/libcuda.so.1
+      ln -sf $out/lib/libnvbufsurface.so.1.0.0 $out/lib/libnvbufsurface.so
+      ln -sf $out/lib/libnvbufsurftransform.so.1.0.0 $out/lib/libnvbufsurftransform.so
+      ln -sf $out/lib/libnvbuf_utils.so.1.0.0 $out/lib/libnvbuf_utils.so
+      ln -sf $out/lib/libnvdsbufferpool.so.1.0.0 $out/lib/libnvdsbufferpool.so
+      ln -sf $out/lib/libnvid_mapper.so.1.0.0 $out/lib/libnvid_mapper.so
+      rm $out/lib/{ld.so.conf,nvidia_icd.json,nvidia.json}
+      chmod +x $out/lib/*
+      runHook postInstall
+    '';
+  };
+  nvidia-icd-json = writers.writeJSON "nvidia_icd.json" {
+    file_format_version = "1.0.0";
+    ICD = {
+      api_version = "1.2.131";
+      library_path = "${library-files}/lib/libGLX_nvidia.so.0";
+    };
+  };
+  nvidia-json = writers.writeJSON "nvidia.json" {
+    file_format_version = "1.0.0";
+    ICD = {
+      library_path = "${library-files}/lib/libEGL_nvidia.so.0";
+    };
+  };
+  combined-files = runCommand "nvidia-l4t-tegra-lib-${nvidia-drivers.version}" {} ''
+    mkdir -p $out/share/{glvnd/egl_vendor.d,vulkan/icd.d}
+    ln -s ${library-files}/lib $out/
+    ln -s ${nvidia-json} $out/share/glvnd/egl_vendor.d/nvidia.json
+    ln -s ${nvidia-icd-json} $out/share/vulkan/icd.d/nvidia_icd.json
   '';
-}
+in
+  combined-files
