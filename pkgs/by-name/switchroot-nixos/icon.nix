f30a271177ae8c5f6bee01c74cb93811ecbb8b66
Waldemar Tomme <waldetomme@gmail.com>
2026-01-07 02:10:39 +0100

    Fix icon

--- pkgs/by-name/switchroot-nixos/icon.nix
+++ pkgs/by-name/switchroot-nixos/icon.nix
@@ -3,14 +3,19 @@
   inputs,
   runCommand,
   imagemagick,
+  resvg,
 }:
 let
   # icons = (import inputs.nixos-artwork { inherit pkgs; }).icons;
   base-svg = "${inputs.nixos-artwork}/logo/nix-snowflake-rainbow.svg";
-  icon-bmp = runCommand "icon.bmp" { } ''
-    ${imagemagick}/bin/magick \
-      ${base-svg} -channel rgba -alpha on -resize 192x192 \
-      BMP3:$out
+  icon-bmp = runCommand "icon.bmp" {
+    buildInputs = [
+      imagemagick
+      resvg
+    ];
+  } ''
+    resvg --width 192 --height 192 ${base-svg} icon.png
+    magick icon.png $out
   '';
 in
 icon-bmp
